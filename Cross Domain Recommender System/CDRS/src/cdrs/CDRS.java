3585d860b9d355c4207b1ad198ef980f2173cb2d
Gaurav Malhotra <gaurav.malhotra@st.niituniversity.in>
2018-12-12 17:00:50 +0530

    Add files via upload

--- Cross Domain Recommender System/CDRS/src/cdrs/CDRS.java
+++ Cross Domain Recommender System/CDRS/src/cdrs/CDRS.java
@@ -1,4 +1,5 @@
 package cdrs;
+import au.com.bytecode.opencsv.CSVReader;
 import java.util.*;
 import java.io.*;
 
@@ -6,45 +7,75 @@ public class CDRS {
      //Movies dataset
      HashMap<String, Integer> moviesDS = new HashMap<>();
      //Declaring independent sets of genres of movies
-     List adventure,drama,mystery,action,comedy,fantasy,thriller,documentary,biography,crime,horror,animation,romance,scifi;
+     List<String> adventure,drama,mystery,action,comedy,fantasy,thriller,documentary,biography,crime,horror,animation,romance,scifi;
      //Books dataset
      HashMap<String, Integer> booksDS = new HashMap<>();
      //Declaring independent sets of genres of books
-     List fiction,romance1,scifi1,autobiography,comic,suspense;
+     List<String> fiction,romance1,scifi1,autobiography,comic,suspense;
      CDRS(){
          //Creating independent sets of genres of movies
-         adventure = new ArrayList();
-         drama = new ArrayList();
-         mystery = new ArrayList();
-         action = new ArrayList();
-         comedy = new ArrayList();
-         fantasy = new ArrayList();
-         thriller = new ArrayList();
-         documentary = new ArrayList();
-         biography = new ArrayList();
-         crime = new ArrayList();
-         horror = new ArrayList();
-         animation = new ArrayList();
-         romance = new ArrayList();
-         scifi = new ArrayList();
+         adventure = new ArrayList<String>();
+         drama = new ArrayList<String>();
+         mystery = new ArrayList<String>();
+         action = new ArrayList<String>();
+         comedy = new ArrayList<String>();
+         fantasy = new ArrayList<String>();
+         thriller = new ArrayList<String>();
+         documentary = new ArrayList<String>();
+         biography = new ArrayList<String>();
+         crime = new ArrayList<String>();
+         horror = new ArrayList<String>();
+         animation = new ArrayList<String>();
+         romance = new ArrayList<String>();
+         scifi = new ArrayList<String>();
          
          //Creating independent sets of genres of movies
-         fiction = new ArrayList();
-         romance1 = new ArrayList();
-         scifi1 = new ArrayList();
-         autobiography = new ArrayList();
-         comic = new ArrayList();
-         suspense = new ArrayList();
+         fiction = new ArrayList<String>();
+         romance1 = new ArrayList<String>();
+         scifi1 = new ArrayList<String>();
+         autobiography = new ArrayList<String>();
+         comic = new ArrayList<String>();
+         suspense = new ArrayList<String>();
      }
      
-     public void PopulateDataSets(){
-         File MDataSet = new File("");
+     public void PopulateDataSets()throws IOException{
+         String FileName="C:\\Users\\gaura\\Documents\\NetBeansProjects\\CDRS\\DataSets\\Book_Dataset.csv";
+         try{
+             FileReader filereader = new FileReader(FileName); //Reading books dataset
+             CSVReader bookcsv = new CSVReader(filereader, ',');// extended jar for dealing with csv files
+           //Filling Data into independent sets of genres of books
+             String bookdata[];
+             while((bookdata = bookcsv.readNext()) != null){
+                 if(bookdata[1].compareTo("Fiction")== 0)
+                     fiction.add(bookdata[0]);
+                 else if(bookdata[1].compareTo("Suspense")== 0)
+                     suspense.add(bookdata[0]);
+                 else if(bookdata[1].compareTo("Comic")== 0)
+                     comic.add(bookdata[0]);
+                 else if(bookdata[1].compareTo("Autobiography")== 0)
+                     autobiography.add(bookdata[0]);
+                 else if(bookdata[1].compareTo("Sci - Fi")== 0)
+                     scifi1.add(bookdata[0]);
+                 else if(bookdata[1].compareTo("Romance")== 0)
+                     romance1.add(bookdata[0]);        
+             }
+             System.out.print(fiction + "\n" + suspense + "\n" + comic + "\n" + autobiography + "\n" + scifi1 +"\n" + romance1);
+             filereader.close();
+         }
+        
+         catch(FileNotFoundException f){
+             f.printStackTrace();
+         }
+         
          
      }
+         
+         
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
        CDRS obj = new CDRS();
+       obj.PopulateDataSets();
         
        
     }
