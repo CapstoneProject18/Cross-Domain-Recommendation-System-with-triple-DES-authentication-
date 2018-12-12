a3b51018be7891125ea1066d775742b4e638e67c
Gaurav Malhotra <gaurav.malhotra@st.niituniversity.in>
2018-12-13 01:11:07 +0530

    Update CDRS.java
    movies of different genres with same name can now be added to different genres

--- Cross Domain Recommender System/CDRS/src/cdrs/CDRS.java
+++ Cross Domain Recommender System/CDRS/src/cdrs/CDRS.java
@@ -14,28 +14,28 @@ public class CDRS {
      List<String> fiction,romance1,scifi1,autobiography,comic,suspense;
      CDRS(){
          //Creating independent sets of genres of movies
-         adventure = new ArrayList<String>();
-         drama = new ArrayList<String>();
-         mystery = new ArrayList<String>();
-         action = new ArrayList<String>();
-         comedy = new ArrayList<String>();
-         fantasy = new ArrayList<String>();
-         thriller = new ArrayList<String>();
-         documentary = new ArrayList<String>();
-         biography = new ArrayList<String>();
-         crime = new ArrayList<String>();
-         horror = new ArrayList<String>();
-         animation = new ArrayList<String>();
-         romance = new ArrayList<String>();
-         scifi = new ArrayList<String>();
+         adventure = new ArrayList<>();
+         drama = new ArrayList<>();
+         mystery = new ArrayList<>();
+         action = new ArrayList<>();
+         comedy = new ArrayList<>();
+         fantasy = new ArrayList<>();
+         thriller = new ArrayList<>();
+         documentary = new ArrayList<>();
+         biography = new ArrayList<>();
+         crime = new ArrayList<>();
+         horror = new ArrayList<>();
+         animation = new ArrayList<>();
+         romance = new ArrayList<>();
+         scifi = new ArrayList<>();
          
          //Creating independent sets of genres of movies
-         fiction = new ArrayList<String>();
-         romance1 = new ArrayList<String>();
-         scifi1 = new ArrayList<String>();
-         autobiography = new ArrayList<String>();
-         comic = new ArrayList<String>();
-         suspense = new ArrayList<String>();
+         fiction = new ArrayList<>();
+         romance1 = new ArrayList<>();
+         scifi1 = new ArrayList<>();
+         autobiography = new ArrayList<>();
+         comic = new ArrayList<>();
+         suspense = new ArrayList<>();
      }
      
      public void PopulateDataSets()throws IOException{
@@ -59,15 +59,66 @@ public class CDRS {
                  else if(bookdata[1].compareTo("Romance")== 0)
                      romance1.add(bookdata[0]);        
              }
-             System.out.print(fiction + "\n" + suspense + "\n" + comic + "\n" + autobiography + "\n" + scifi1 +"\n" + romance1);
+             System.out.print(fiction + "\n" + suspense + "\n" + comic + "\n" + autobiography + "\n" + scifi1 +"\n" + romance1 +"\n");
+             
              filereader.close();
+             System.out.print(bookcsv);
          }
+        
         
          catch(FileNotFoundException f){
              f.printStackTrace();
          }
-         
-         
+        
+        String FileName2="C:\\Users\\gaura\\Documents\\NetBeansProjects\\CDRS\\DataSets\\Movies_Database.csv";
+          try{
+             FileReader filereader1 = new FileReader(FileName2); //Reading books dataset
+             CSVReader moviecsv = new CSVReader(filereader1, ',');// extended jar for dealing with csv files
+           //Filling Data into independent sets of genres of books
+             String moviedata[];
+             while((moviedata = moviecsv.readNext()) != null){
+                 if(moviedata[2].contains("Adventure"))
+                     adventure.add(moviedata[1]);
+                  if(moviedata[2].contains("Drama"))
+                     drama.add(moviedata[1]);
+                  if(moviedata[2].contains("Mystery"))
+                     mystery.add(moviedata[1]);
+                  if(moviedata[2].contains("Action"))
+                     action.add(moviedata[1]);
+                  if(moviedata[2].contains("Comedy"))
+                     comedy.add(moviedata[1]);
+                  if(moviedata[2].contains("Fantasy"))
+                     fantasy.add(moviedata[1]);     
+                  if(moviedata[2].contains("Thriller"))
+                     thriller.add(moviedata[1]);
+                   if(moviedata[2].contains("Documentary"))
+                     documentary.add(moviedata[1]);
+                  if(moviedata[2].contains("Biography"))
+                     biography.add(moviedata[1]);
+                  if(moviedata[2].contains("Crime"))
+                     crime.add(moviedata[1]);
+                  if(moviedata[2].contains("Horror"))
+                     horror.add(moviedata[1]);
+                  if(moviedata[2].contains("Animation"))
+                     animation.add(moviedata[1]);
+                  if(moviedata[2].contains("Romance"))
+                     romance.add(moviedata[1]);
+                  if(moviedata[2].contains("Sci-Fi"))
+                     scifi.add(moviedata[1]);
+    
+             }
+             System.out.print("\n");
+             System.out.print(horror);
+             System.out.print("\n");
+             System.out.print(drama);
+             filereader1.close();
+         }
+        
+        
+         catch(FileNotFoundException f){
+             f.printStackTrace();
+         }
+      
      }
          
          
