776c18fb9704a01615282a1e23233bd2339e6dc4
Gaurav Malhotra <gaurav.malhotra@st.niituniversity.in>
2018-12-16 21:02:49 +0530

    Update CDRS.java

--- Cross Domain Recommender System/CDRS/src/cdrs/CDRS.java
+++ Cross Domain Recommender System/CDRS/src/cdrs/CDRS.java
@@ -4,12 +4,8 @@ import java.util.*;
 import java.io.*;
 
 public class CDRS {
-     //Movies dataset
-     HashMap<String, Integer> moviesDS = new HashMap<>();
      //Declaring independent sets of genres of movies
      List<String> adventure,drama,mystery,action,comedy,fantasy,thriller,documentary,biography,crime,horror,animation,romance,scifi;
-     //Books dataset
-     HashMap<String, Integer> booksDS = new HashMap<>();
      //Declaring independent sets of genres of books
      List<String> fiction,romance1,scifi1,autobiography,comic,suspense;
      //Declaring a flag variable
@@ -94,58 +90,72 @@ public class CDRS {
                  if(moviedata[2].contains("Adventure")){
                      adventure.add(moviedata[1]);
                      adventure.add(moviedata[3]);
+                     adventure.add("Adventure");
                  }
                   if(moviedata[2].contains("Drama")){
                      drama.add(moviedata[1]);
                      drama.add(moviedata[3]);
+                     drama.add("Drama");
                   }
                   if(moviedata[2].contains("Mystery")){
                      mystery.add(moviedata[1]);
                      mystery.add(moviedata[3]);
+                     mystery.add("Mystery");
                   }
                   if(moviedata[2].contains("Action")){
                      action.add(moviedata[1]);
                      action.add(moviedata[3]);
+                     mystery.add("Action");
                   }
                   if(moviedata[2].contains("Comedy")){
                      comedy.add(moviedata[1]);
                      comedy.add(moviedata[3]);
+                     comedy.add("Comedy");
                   }
                   if(moviedata[2].contains("Fantasy")){
                      fantasy.add(moviedata[1]);
                      fantasy.add(moviedata[3]);
+                     fantasy.add("Fantasy");
                   }
                   if(moviedata[2].contains("Thriller")){
                      thriller.add(moviedata[1]);
                      thriller.add(moviedata[3]);
+                     thriller.add("Thriller");
                   }
                    if(moviedata[2].contains("Documentary")){
                      documentary.add(moviedata[1]);
                      documentary.add(moviedata[3]); 
+                     documentary.add("Documentary");
                    }
                   if(moviedata[2].contains("Biography")){
                      biography.add(moviedata[1]);
                      biography.add(moviedata[3]);
+                     biography.add("Biography");
                   }
                   if(moviedata[2].contains("Crime")){
                      crime.add(moviedata[1]);
-                     crime.add(moviedata[3]);  
+                     crime.add(moviedata[3]);
+                     crime.add("Crime");
                   }
                   if(moviedata[2].contains("Horror")){
                      horror.add(moviedata[1]);
-                     horror.add(moviedata[3]);  
+                     horror.add(moviedata[3]);
+                     horror.add("Horror");
                   }
                   if(moviedata[2].contains("Animation")){
                      animation.add(moviedata[1]);
                      animation.add(moviedata[3]);
+                     animation.add("Animation");
                   }
                   if(moviedata[2].contains("Romance")){
                      romance.add(moviedata[1]);
-                     romance.add(moviedata[3]);  
+                     romance.add(moviedata[3]);
+                     romance.add("Romance");
                   }
                   if(moviedata[2].contains("Sci-Fi")){
                      scifi.add(moviedata[1]);
                      scifi.add(moviedata[3]);
+                     scifi.add("Sci-Fi");
                   }
              }
              filereader1.close();
@@ -158,19 +168,21 @@ public class CDRS {
       
      }
      
-     
+     // Searching user's genre in books database
   public int GenreSearch(List <String> temp, String userInput){
-      int i = 0;
-      while( i++ < temp.size()) 
+      int i = 0, j=i + 1;
+      while( i < temp.size()/2) 
         { 
-            if(userInput.equals(temp.get(0))) {
-                BookRating = Float.parseFloat(temp.get(1));
+            if(userInput.equals(temp.get(2*i))) {
+                BookRating = Float.parseFloat(temp.get((2*j) - 1));
                 return (flag = 1);
             } 
+            i++;
         }
       return flag;
   }
-         
+       //checking if user's book exists in database
+       //if it does then storing the genre of the book
    public void mappingDataSets(String userInput){
    String UserGenre;
    while(true){
@@ -187,7 +199,7 @@ public class CDRS {
            break;
    }
        else if(GenreSearch(autobiography,userInput)== 1){
-           UserGenre = "Fiction";
+           UserGenre = "Autobiography";
            break;
    }
        else if(GenreSearch(scifi1,userInput)== 1){
@@ -199,17 +211,83 @@ public class CDRS {
            break;
    }
        else{
-           UserGenre = "Not in database";
+           UserGenre = " not in database";
            break;
    }
    }
    System.out.println("\nGenre of the queried book is " + UserGenre + "\nRating of the queried book is " + BookRating);
-   ResultSet();
+   ResultSet(UserGenre);
    }
-
-   public void ResultSet(){
-       
+     //mapping book genre with movie genre
+   public void ResultSet(String userGenre){
+       if((adventure.get(2)).equals(userGenre)){
+           printRecommendations(adventure);
+       }
+       else if(drama.get(2).equals(userGenre)){
+           printRecommendations(drama);
+       }
+       else if(mystery.get(2).equals(userGenre)){
+           printRecommendations(mystery);
+       }
+       else if(action.get(2).equals(userGenre)){
+           printRecommendations(action);
+       }
+       else if(comedy.get(2).equals(userGenre)){
+           printRecommendations(comedy);
+       }
+       else if(fantasy.get(2).equals(userGenre)){
+           printRecommendations(fantasy);
+       }
+       else if(thriller.get(2).equals(userGenre)){
+           printRecommendations(thriller);
+       }
+        else if(biography.get(2).equals(userGenre)){
+           printRecommendations(biography);
+       }
+        else if(crime.get(2).equals(userGenre)){
+           printRecommendations(crime);
+       }
+        else if(horror.get(2).equals(userGenre)){
+           printRecommendations(horror);
+       }
+       else if(animation.get(2).equals(userGenre)){
+           printRecommendations(animation);
+       }
+        else if(romance.get(2).equals(userGenre)){
+           printRecommendations(romance);
+       }
+        else if((scifi.get(2)).equals(userGenre)){
+           printRecommendations(scifi);
+            }
+       else
+        {
+            printRecommendations(adventure);
+             
+        }
+    
    }
+   //printing 3 random recommended movies between rating 3.5 and 4.8
+   public void printRecommendations(List <String> temp )
+   {
+       Random r = new Random(); 
+       List <Integer> unique = new ArrayList<>();
+       int count =0, length = temp.size()/3 - 1, min = 0,i;
+       System.out.println("Movies Recommendations For you are:");
+          while(count < 3){
+              i = (int)(Math.random() * ((length - 1) - min)) + min;
+              if(!unique.contains(i))
+               if(Float.parseFloat(temp.get((3*(i+1)) -2)) <= 4.8 && Float.parseFloat(temp.get(3*(i+1) -2)) >= 2 && count < 3.5){
+                   System.out.println("Movie Name: " +temp.get(3*i) + " Rating:" + temp.get(3*(i+1) -2));
+                   count++;
+                   unique.add(i);
+                    }
+         }
+   
+         }
+             
+   
+   
+  // main function 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Book Name");
