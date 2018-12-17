64e1cebe1d2a0c471f8530907c4a786fe0450ed0
KashishChaurasia <kashish.chaurasia@st.niituniversity.in>
2018-12-17 20:44:34 +0530

    Add files via upload

---
+++ MRSown_singleDomain/src/main/java/com/webIntelligence/movieRecommendationSystem/model/LoginForm.java
@@ -0,0 +1,25 @@
+package com.webIntelligence.movieRecommendationSystem.model;
+
+public class LoginForm {
+	
+	private String email;
+	private String password;
+	public LoginForm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
---
+++ MRSown_singleDomain/src/main/java/com/webIntelligence/movieRecommendationSystem/model/Rating.java
@@ -0,0 +1,24 @@
+package com.webIntelligence.movieRecommendationSystem.model;
+
+public class Rating {
+	private String movieName;
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	private int rating;
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+
+}
